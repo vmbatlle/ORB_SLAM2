29eb1b629e974aa425f729d281e9a44107984573
Victor M. Batlle <736478@unizar.es>
2020-04-08 18:52:46 +0200

    Addapt Stereo example to export KeyPoints detected and its location in the world

--- Examples/Stereo/stereo_kitti.cc
+++ Examples/Stereo/stereo_kitti.cc
@@ -61,6 +61,9 @@ int main(int argc, char **argv)
     cout << "Start processing sequence ..." << endl;
     cout << "Images in the sequence: " << nImages << endl << endl;   
 
+    ofstream f;
+    f.open("FrameKeyPoints.txt");
+
     // Main loop
     cv::Mat imLeft, imRight;
     for(int ni=0; ni<nImages; ni++)
@@ -84,7 +87,7 @@ int main(int argc, char **argv)
 #endif
 
         // Pass the images to the SLAM system
-        SLAM.TrackStereo(imLeft,imRight,tframe);
+        cv::Mat Tcw = SLAM.TrackStereo(imLeft,imRight,tframe);
 
 #ifdef COMPILEDWITHC11
         std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
@@ -93,6 +96,27 @@ int main(int argc, char **argv)
 #endif
 
         double ttrack= std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
+
+        if (SLAM.GetTrackingState() == ORB_SLAM2::Tracking::OK) {
+            std::vector<cv::KeyPoint> mvKeysUn = SLAM.GetTrackedKeyPointsUn();
+            std::vector<ORB_SLAM2::MapPoint*> mvpMapPoints = SLAM.GetTrackedMapPoints();
+            const int n = mvKeysUn.size();
+            const int matches = std::count_if(mvpMapPoints.begin(), mvpMapPoints.end(), [](ORB_SLAM2::MapPoint* i) {return i != nullptr && i->Observations()>0;});
+            f << fixed;
+            f << setprecision(6) << ni << " " << tframe << " " << matches << endl;
+            for(int i=0;i<n;i++)
+            {
+                ORB_SLAM2::MapPoint* pMP = mvpMapPoints[i];
+                if(pMP)
+                {
+                    if(pMP->Observations()>0) {
+                        cv::Point2f pt = mvKeysUn[i].pt;
+                        cv::Mat pos = pMP->GetWorldPos();
+                        f << setprecision(7) << pt.x << " " << pt.y << " " << pos.at<float>(0) << " " << pos.at<float>(1) << " " << pos.at<float>(2) << endl;
+                    }
+                }
+            }
+        }
 
         vTimesTrack[ni]=ttrack;
 
@@ -106,6 +130,8 @@ int main(int argc, char **argv)
         if(ttrack<T)
             usleep((T-ttrack)*1e6);
     }
+
+    f.close();
 
     // Stop all threads
     SLAM.Shutdown();
